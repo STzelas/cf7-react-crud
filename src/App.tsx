7c862808470b399a4a9b57877ce89f0acd6704df
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-29 20:14:46 +0300

    chore: removed code

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,16 @@ import ProductListPage from "@/pages/ProductListPage.tsx";
 import ProductPage from "@/pages/ProductPage.tsx";
 import NotFoundPage from "@/pages/NotFoundPage.tsx";
 import Layout from "@/components/Layout.tsx";
-import { AuthProvider } from "@/context/AuthProvider.tsx";
-import LoginPage from "@/pages/LoginPage.tsx";
-import ProtectedRoute from "@/components/ProtectedRoute.tsx";
 
 function App() {
   return (
     <>
-      <AuthProvider>
+
         <BrowserRouter>
           <Routes>
             <Route element={<Layout />}>
               <Route index element={<HomePage />} />
-              <Route path="login" element={<LoginPage />} />
-
-              <Route path="products" element={<ProtectedRoute />}>
+              <Route path="products">
                 <Route index element={<ProductListPage />} />
                 <Route path="new" element={<ProductPage mode="create" />} />
                 <Route
@@ -27,13 +22,11 @@ function App() {
                   element={<ProductPage mode="edit" />}
                 />
               </Route>
-
               <Route path="*" element={<NotFoundPage />} />
             </Route>
           </Routes>
         </BrowserRouter>
         <Toaster richColors />
-      </AuthProvider>
     </>
   );
 }
