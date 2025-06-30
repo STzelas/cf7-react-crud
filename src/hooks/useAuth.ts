330c8e6354eec08069803d0de98c68b0c6540463
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-30 16:46:05 +0300

    fix: fixed import

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext} from "@/context/AuthContext.tsx";
+import { AuthContext} from "@/context/AuthContext.ts";
 
 export function useAuth() {
   const ctx = useContext(AuthContext);
